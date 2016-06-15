3ad0e9bf220ec6742b3523828329eedd08dd4a84
YaYaChen827 <suck827@gmail.com>
2016-06-16 03:36:53 +0800

    Add files via upload

---
+++ Lesson5_Quiz_Predicting_Run_Time.py
@@ -0,0 +1,17 @@
+import time
+
+def time_execution(code):
+	
+	start = time.clock()
+	#The eval function lets the python program to run python code.
+	#The code format is string (ex: eval('print 1'))
+	result = eval(code)
+	#Get execution time
+	run_time = time.clock()
+	return result, run_time
+
+#Testing time function
+def spin_loop(n):
+	i = 0
+	while i < n:
+		i += 1
