4f854f3effcf82f12d7a31af3eed3755e013842f
YaYaChen827 <suck827@gmail.com>
2016-06-18 11:59:12 +0800

    Define a Dictionary

---
+++ Quiz/Lesson5_Quiz_Dict_Population.py
@@ -0,0 +1,19 @@
+# Define a Dictionary, population,
+# that provides information
+# on the world's largest cities.
+# The key is the name of a city
+# (a string), and the associated
+# value is its population in
+# millions.
+
+#   Key     |   Value
+# Shanghai  |   17.8
+# Istanbul  |   13.3
+# Karachi   |   13.0
+# Mumbai    |   12.5
+
+# Each dictionary has a key and value (dict_ = { <key> : <value>, ... })
+population = {'Shanghai':17.8, 'Istanbul':13.3, 'Karachi':13.0, 'Mumbai':12.5}
+
+print population
+#>>> {'Karachi': 13.0, 'Shanghai': 17.8, 'Istanbul': 13.3, 'Mumbai': 12.5}
